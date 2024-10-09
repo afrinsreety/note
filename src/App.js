289846460bd9061de148fe337f0a97142c4c8332
Afrin Zahan Sreety <zahansreety@gmail.com>
2024-10-09 10:07:57 +0600

    display notes

--- src/App.js
+++ src/App.js
@@ -1,56 +1,65 @@
 import './App.css';
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 function App() {
- const [title, setTitle] = useState("1st note");
- const [note,setnote] = useState();
- const [notes, setnotes] = useState([]);
- function changeTitle(event){
-  setTitle(event.target.value);
-  console.log("changing..", event.target.value);
- }
- function changeNote(event){
-  setnote(event.target.value);
-  console.log("changing...",event.target.value);
- }
+	// declaring stats 
+	const [title, setTitle] = useState("");
+	const [note, setnote] = useState("");
+	const [notes, setnotes] = useState([]);
 
- function onSave(){
-  console.log("on save", title, note);
+	// declaring functions 
+	function changeTitle(event) {
+		setTitle(event.target.value);
+		console.log("changing..", event.target.value);
+	}
+	function changeNote(event) {
+		setnote(event.target.value);
+		console.log("changing...", event.target.value);
+	}
 
-  console.log("notes", notes);
-  debugger
-  var localNotes = notes;
-  
-  localNotes.push({
-    title: title,
-    note: note
-  })
-  setnotes(localNotes);
-  console.log("notes", notes);
+	function onSave() {
+		var currentNote = {
+			title: title,
+			note: note
+		}
+		setnotes([...notes, currentNote]);
+	}
 
+	return (
+		<div className='container'>
+			<h3>Create a note </h3>
+			<div className='formContainer'>
+				<div className='input-container'>
+					<label>Title: </label>
+					<input onChange={changeTitle} />
+				</div>
+				<div className='input-container'>
+					<label>Note : </label>
+					<textarea onChange={changeNote}></textarea>
+				</div>
+				<div className='input-container'>
+					<button className='btn-save' onClick={onSave}>Save</button>
+				</div>
+			</div>
 
- }
-  return (
-    <div>
-      <h3>Create a note : {title}</h3>
-
-      <div className='input-container'>
-        <label>Title: </label>
-        <input onChange={changeTitle}  />
-      </div>
-<h3>Create a note : {note}</h3>
-       <div className='input-container'>
-        <label>Note : </label>
-        <textarea onChange={changeNote}></textarea>
-      </div> 
-
-      <div className='input-container'>
-        <button onClick={onSave}>Save</button>
-      </div> 
-
-
-    </div>
-  );
+			<div className='output-container' >
+				<table className='data-table' border="2">
+					<tr>
+						<th>Title</th>
+						<th>Note</th>
+					</tr>
+					{
+						notes.map(function (item) {
+							return (<tr>
+								<td>{item.title}</td>
+								<td>{item.note}</td>
+							</tr>)
+						})
+					}
+				</table>
+			</div>
+		</div>
+	);
 }
 
 export default App;
